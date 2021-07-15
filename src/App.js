8ee1d3cda7c26ec42d479ee435d03486d08ae3e5
DenisFanteria <denis.fanteria@gmail.com>
2021-07-15 20:11:27 +0200

    debut menu et responsive

--- src/App.js
+++ src/App.js
@@ -1,60 +1,75 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
 import gsap from "gsap";
 import "./styles/App.scss";
 import Header from "./components/header";
-import Banner from "./components/banner";
-import Cases from "./components/cases";
-import IntroOverlay from "./components/introOverlay";
+
+//pages
+import Home from "./pages/home";
+import CaseStudies from "./pages/caseStudies";
+import Approach from "./pages/approach";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Navigation from "./components/navigation";
+
+//routes
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  { path: "/case-studies", name: "Case Studies", Component: CaseStudies },
+  { path: "/approach", name: "Approach", Component: Approach },
+  { path: "/services", name: "Services", Component: Services },
+  { path: "/about-us", name: "About Us", Component: About },
+];
+
+function debounce(fn, ms) {
+  let timer;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
 
 function App() {
+  //empêche les flash lors des fast reload
+  gsap.to("body", 0, { css: { visibility: "visible" } });
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
+    // Calcul de la taille des écrans mobiles (sans barres d'outils)
+    let vh = dimensions.height * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+    //Permet de mettre à jour la taille de l'écran dès qu'elle change.
 
-    gsap.to("body", 0, { css: { visibility: "visible" } });
-
-    //timeline
-    const tl = gsap.timeline();
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 1,
-      skewY: 7,
-      stagger: {
-        amount: 0.3,
-      },
-    })
-      .to(".overlay-top", 1.6, {
-        height: 0,
-        ease: "expo.inOut",
-        stagger: { amount: 0.4 },
-      })
-      .to(".overlay-bottom", 1.6, {
-        width: 0,
-        ease: "expo-inOut",
-        delay: -0.8,
-        stagger: {
-          amount: 0.4,
-        },
-      })
-      .to(".intro-overlay", 0, { css: { display: "none" } })
-      .from(".case-image img", 1.6, {
-        scale: 1.4,
-        ease: "expo-inOut",
-        delay: -2,
-        stagger: {
-          amount: 0.4,
-        },
+    const debounceHandleResize = debounce(function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth,
       });
-  }, []);
+    }, 1000);
 
+    window.addEventListener("resize", debounceHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debounceHandleResize);
+    };
+  });
   return (
-    <div className="App">
-      <IntroOverlay />
+    <>
       <Header />
-      <Banner />
-      <Cases />
-    </div>
+      <div className="App">
+        {routes.map(({ path, Component }) => (
+          <Route key={"path"} exact path={path}>
+            <Component />
+          </Route>
+        ))}
+      </div>
+      <Navigation />
+    </>
   );
 }
 
