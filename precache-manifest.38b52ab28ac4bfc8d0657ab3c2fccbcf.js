b45604764a2da4f84ba7a05c8f6dd72f886a5c62
DenisFanteria <denis.fanteria@gmail.com>
2021-07-20 22:48:42 +0200

    Updates

---
+++ precache-manifest.38b52ab28ac4bfc8d0657ab3c2fccbcf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9922d90c9bfe48eb112da71e52d97fce",
+    "url": "/ar-episode1/index.html"
+  },
+  {
+    "revision": "dcd1e7ca0894b09db926",
+    "url": "/ar-episode1/static/css/main.38f78e72.chunk.css"
+  },
+  {
+    "revision": "756cfc3dc9b0f01981c2",
+    "url": "/ar-episode1/static/js/2.a419798b.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/ar-episode1/static/js/2.a419798b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd1e7ca0894b09db926",
+    "url": "/ar-episode1/static/js/main.3111e675.chunk.js"
+  },
+  {
+    "revision": "65d579cd12e215f1e471",
+    "url": "/ar-episode1/static/js/runtime-main.c85519c1.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/ar-episode1/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/ar-episode1/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/ar-episode1/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/ar-episode1/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "24e8d8f29296438b099443772c822a1f",
+    "url": "/ar-episode1/static/media/up-arrow-circle.24e8d8f2.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/ar-episode1/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
